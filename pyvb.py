d087452a9af1be143f1d5845d4593ed39e77b646
aaronkirsh <aaronkirsh@gmail.com>
2016-10-01 13:00:42 -0700

    Update pyvb.py
    Add path to VBoxManage and import subprocess

--- pyvb.py
+++ pyvb.py
@@ -1,3 +1,10 @@
 #! python3
 
 #pyvb.py - Start, stop, and restart virtualbox machines
+
+paths = { \
+	'VBoxManage'	: r'C:\Program Files\Oracle\VirtualBox\VBoxManage' \
+		}
+
+import subprocess
+	
